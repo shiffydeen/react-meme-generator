f0b9ffaae7c13e0630a258fde754158c5efa3298
Sheriff Aderombi <ayaderombi@gmail.com>
2024-10-08 14:21:26 +0100

    meme-generator

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header.jsx'
+import Meme from './components/Meme.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  
+
+  return (
+    <div class="app">
+       <Header />
+       <Meme />
+       <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <small>
+          <a
+            className="footer--link"
+            href="https://www.linkedin.com/in/aderombi-sheriff-388033177/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sheriff Aderombi - 2024
+          </a>
+        </small>
+      </footer>
+    );
+  }
